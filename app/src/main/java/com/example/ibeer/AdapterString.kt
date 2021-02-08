1807c403e52eb72808817a876cfccd7e0c204fc5
JaimeCuenca <jaime.cuenca.caro@gmail.com>
2021-02-08 14:21:20 +0100

    hecho el listener del recycledview, falta mirar el meter un id manualmente por el fallo del cast

--- app/src/main/java/com/example/ibeer/AdapterString.kt
+++ app/src/main/java/com/example/ibeer/AdapterString.kt
@@ -13,24 +13,28 @@ import kotlinx.coroutines.withContext
 class AdapterString : RecyclerView.Adapter<AdapterString.StringViewHolder>(){
 
     private var cervezas : List<Cerveza>? = null
+    private lateinit var recyclerView: RecyclerView
 
-    class StringViewHolder(root: View, val textView: TextView, val imagen: ImageView) : RecyclerView.ViewHolder(root)
+    class StringViewHolder(var root: View, val textView: TextView, val imagen: ImageView) : RecyclerView.ViewHolder(root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StringViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.activity_main_item, parent, false)
-
         val textView = view.findViewById<TextView>(R.id.tv_activity_main_item)
         val imagen = view.findViewById<ImageView>(R.id.im_activity_main_item)
         return StringViewHolder(view, textView, imagen)
     }
 
-    override fun onBindViewHolder(holder: AdapterString.StringViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: StringViewHolder, position: Int) {
         cervezas?.let {
             holder.textView.text = it[position].name
             if(it[position].image_url!="https://images.punkapi.com/v2/keg.png") {
                 Picasso.get().load(it[position].image_url).into(holder.imagen)
             }else{
                 Picasso.get().load("https://i1.wp.com/cervecing.es/wp-content/uploads/404-error-beer-not-found.png?fit=178%2C178&ssl=1).into(holder.imagen").into(holder.imagen)
+            }
+            holder.root.setOnClickListener{
+                val pos = holder.adapterPosition
+                DetailsActivity.createDetailsActivity(holder.root.context , pos)
             }
         }
     }
@@ -40,6 +44,11 @@ class AdapterString : RecyclerView.Adapter<AdapterString.StringViewHolder>(){
             return it.size
         }
         return 0
+    }
+
+    override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
+        super.onAttachedToRecyclerView(recyclerView)
+        this.recyclerView = recyclerView
     }
 
     suspend fun setData(string: List<Cerveza>){
--- app/src/main/java/com/example/ibeer/DetailsActivity.kt
+++ app/src/main/java/com/example/ibeer/DetailsActivity.kt
@@ -1,6 +1,5 @@
 package com.example.ibeer
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
@@ -25,42 +24,62 @@ class DetailsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        val id = intent.getIntExtra("id", 0)
         model = ViewModelProvider(this).get(DetailsActivityViewModel::class.java)
-
+        detallesCerveza(id)
         binding.bBuscar.setOnClickListener {
-            binding.progressBar.visibility = View.VISIBLE
-            GlobalScope.launch(Dispatchers.IO) {
-                val resultado = model.getSingleItem(binding.etDetails.text.toString())
-                withContext(Dispatchers.Main) {
-                    binding.tvResultados.text = resultado?.get(0)?.name
-                    if(resultado?.get(0)?.image_url!="https://images.punkapi.com/v2/keg.png") {
-                        Picasso.get().load(resultado?.get(0)?.image_url).into(binding.ivDetalle)
-                    }else{
-                        Picasso.get().load("https://i1.wp.com/cervecing.es/wp-content/uploads/404-error-beer-not-found.png?fit=178%2C178&ssl=1).into(holder.imagen").into(binding.ivDetalle)
+            detallesCerveza(binding.etDetails.text )
+        }
+    }
+
+    fun detallesCerveza(id: Int){
+        id?.let {
+            if (it in 1..25) {
+                binding.progressBar.visibility = View.VISIBLE
+                GlobalScope.launch(Dispatchers.IO) {
+                    val resultado = model.getSingleItem((it+1).toString())
+                    withContext(Dispatchers.Main) {
+                        binding.tvResultados.text = resultado?.get(0)?.name
+                        if (resultado?.get(0)?.image_url != "https://images.punkapi.com/v2/keg.png") {
+                            Picasso.get().load(resultado?.get(0)?.image_url).into(binding.ivDetalle)
+                        } else {
+                            Picasso.get()
+                                .load("https://i1.wp.com/cervecing.es/wp-content/uploads/404-error-beer-not-found.png?fit=178%2C178&ssl=1).into(holder.imagen")
+                                .into(binding.ivDetalle)
+                        }
+                        var ingredientes = ""
+                        for (i in resultado?.get(0)?.ingredients?.malt!!) {
+                            ingredientes = ingredientes + " " + i.name + ","
+                        }
+                        for (i in resultado[0].ingredients.hops) {
+                            ingredientes = ingredientes + " " + i.name + ","
+                        }
+                        ingredientes = ingredientes + " " + resultado.get(0).ingredients.yeast
+                        binding.tvResultados2.text =
+                            "DESCRIPCIÓN:\n" + resultado[0].description + "\n\nGRADUACIÓN:\n" + resultado[0].abv + "º" + "\n\nINGREDIENTES:\n" + ingredientes.substring(
+                                1
+                            )
+                        binding.ivDetalle.isVisible = true
+                        binding.tvResultados.isVisible = true
+                        binding.tvResultados2.isVisible = true
+                        binding.progressBar.visibility = View.GONE
                     }
-                    var ingredientes =""
-                    for (i in resultado?.get(0)?.ingredients?.malt!!){
-                        ingredientes=ingredientes+" "+i.name+","
-                    }
-                    for (i in resultado[0].ingredients.hops){
-                        ingredientes=ingredientes+" "+i.name+","
-                    }
-                    ingredientes=ingredientes+" "+ resultado.get(0).ingredients.yeast
-                    binding.tvResultados2.text =  "DESCRIPCIÓN:\n"+ resultado[0].description +"\n\nGRADUACIÓN:\n"+ resultado[0].abv +"º"+"\n\nINGREDIENTES:\n"+ingredientes.substring(1)
-                    binding.ivDetalle.isVisible = true
-                    binding.tvResultados.isVisible = true
-                    binding.tvResultados2.isVisible = true
-                    binding.progressBar.visibility = View.GONE
                 }
             }
         }
     }
+
     companion object {
 
         fun createDetailsActivity(context : Context) {
             val intent = Intent(context, DetailsActivity::class.java)
             context.startActivity(intent)
         }
+        fun createDetailsActivity(context: Context, pos: Int) {
+            val intent = Intent(context, DetailsActivity::class.java)
+            intent.putExtra("id", pos)
+            context.startActivity(intent)
+        }
+
     }
 }
